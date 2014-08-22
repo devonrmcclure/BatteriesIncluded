81491e1892b2d3bdb3b1da0ad8080b532e1592e6
Phinocio <devon.r.mcclure@gmail.com>
2014-08-21 19:42:21 -0700

    Fixed catalog view not showing properly when another row of items is added

--- app/views/catalog.blade.php
+++ app/views/catalog.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 
-	<div class="col-md-2 ">
+	<div class="col-md-2">
     <h2>Categories</h2>
     <div >
       <ul class="catalog-categories">
@@ -21,7 +21,6 @@
     <div class="col-md-12">
       <h2>Newest Items</h2>
     </div>
-    <div class="row content">
       @foreach($products as $product)
         <div class="col-md-3">
         @if($product->price != 0.00)
@@ -33,16 +32,55 @@
           @if($product->subcategory->subcategory_name != 'Uncategorized')
             <small><b>Category:</b> <a href="#"> {{ $product->subcategory->subcategory_name }} </a></small>
           @elseif($product->category->category_name)
-            <small><b>Category:</b> <a href="#"> {{ $product->category->category_name }} </a></small>
+            <small><b>Category:</b> <a href="#">{{ $product->category->category_name }}</a></small>
           @endif
           <h4 class="product-name">{{ $product->product_name }}</h4>
           <p>
             {{ $product->product_description }}
           </p>
-
         </div>
       @endforeach
-    </div>
+
+      @foreach($products as $product)
+        <div class="col-md-3">
+        @if($product->price != 0.00)
+          <span class="pull-right call-for-price">${{ $product->price }}</span>
+        @else
+          <span class="pull-right call-for-price">Call for price</span>
+        @endif
+          <img src="http://placehold.it/100x100" class="img-responsive"/>
+          @if($product->subcategory->subcategory_name != 'Uncategorized')
+            <small><b>Category:</b> <a href="#"> {{ $product->subcategory->subcategory_name }} </a></small>
+          @elseif($product->category->category_name)
+            <small><b>Category:</b> <a href="#">{{ $product->category->category_name }}</a></small>
+          @endif
+          <h4 class="product-name">{{ $product->product_name }}</h4>
+          <p>
+            {{ $product->product_description }}
+          </p>
+        </div>
+      @endforeach
+
+      <!-- Here for tessting purposes because I only have 4 products in the database atm -->
+      @foreach($products as $product)
+        <div class="col-md-3">
+        @if($product->price != 0.00)
+          <span class="pull-right call-for-price">${{ $product->price }}</span>
+        @else
+          <span class="pull-right call-for-price">Call for price</span>
+        @endif
+          <img src="http://placehold.it/100x100" class="img-responsive"/>
+          @if($product->subcategory->subcategory_name != 'Uncategorized')
+            <small><b>Category:</b> <a href="#"> {{ $product->subcategory->subcategory_name }} </a></small>
+          @elseif($product->category->category_name)
+            <small><b>Category:</b> <a href="#">{{ $product->category->category_name }}</a></small>
+          @endif
+          <h4 class="product-name">{{ $product->product_name }}</h4>
+          <p>
+            {{ $product->product_description }}
+          </p>
+        </div>
+      @endforeach
   </div>
 
 @stop
