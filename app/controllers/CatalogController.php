c20cef25ac5177095d7b1dce93b2efe7eac1cbec
Phinocio <devon.r.mcclure@gmail.com>
2014-08-22 22:39:03 -0700

    Made category links show properly based on what category page the use is on

--- app/controllers/CatalogController.php
+++ app/controllers/CatalogController.php
@@ -10,9 +10,17 @@ class CatalogController extends \BaseController {
 	public function index()
 	{
         $data = Category::orderBy('category_name', 'ASC')->get(); // Get all category names and order them Alphabetically.
+
+        foreach($data as $i)
+        {
+            $subCatLinks = Subcategory::orderBy('subcategory_name', 'ASC')->whereparent_category($i->id)->get();
+        }
+
 		return View::make('catalog')
             ->with('products', Product::paginate(16))
             ->with('categories', $data)
+            ->with('subCategories', NULL)
+            ->with('subCategoryLinks', $subCatLinks)
             ->with('categoryLinks', $data);
 
 	}
@@ -24,6 +32,11 @@ class CatalogController extends \BaseController {
         {
             $categoryLinks = Category::orderBy('category_name', 'ASC')->get();
             $data = Category::wherecategory_name($category)->get();
+
+            foreach($data as $i)
+            {
+                $subCatLinks = Subcategory::orderBy('subcategory_name', 'ASC')->whereparent_category($i->id)->get();
+            }
             foreach($data as $i)
             {
                 $products = Product::orderBy('product_name', 'ASC')->wherecategory_id($i->id)->paginate(16);
@@ -32,29 +45,36 @@ class CatalogController extends \BaseController {
                 ->with('products', $products)
                 ->with('categories', $data)
                 ->with('subCategories', NULL)
+                ->with('subCategoryLinks', $subCatLinks)
                 ->with('categoryLinks', $categoryLinks);
 
         } elseif(count(Subcategory::wheresubcategory_name($category)->get()) != 0)
         {
             $categoryLinks = Category::orderBy('category_name', 'ASC')->get();
             $data = Subcategory::wheresubcategory_name($category)->get();
+
+            foreach($data as $i)
+            {
+                $subCatLinks = Subcategory::orderBy('subcategory_name', 'ASC')->whereparent_category($i->parent_category)->get();
+            }
+            foreach($data as $i)
+            {
+                $parentCat = Category::whereid($i->parent_category)->get();
+            }
             foreach($data as $i)
             {
                 $products = Product::orderBy('product_name', 'ACS')->wheresubcategory_id($i->id)->paginate(16);
             }
+
             return View::make('catalog')
                 ->with('products', $products)
-                ->with('categories', NULL)
+                ->with('categories', $parentCat)
                 ->with('subCategories', $data)
+                ->with('subCategoryLinks', $subCatLinks)
                 ->with('categoryLinks', $categoryLinks);
         } else {
             // 404 error.
             return 'Hi';
         }
-    }
-
-    public function showSubCategory($subCategory)
-    {
-
     }
 }
--- app/views/catalog.blade.php
+++ app/views/catalog.blade.php
@@ -6,49 +6,72 @@
 
 @section('content')
 
-	<div class="col-md-2">
-    <h2>Categories</h2> <hr />
+	<div class="col-md-3">
+    <h2>
+      @if(count($categories) == 1 && count($subCategories) != 1)
+        @foreach($categories as $category)
+          {{ $category->category_name }}
+        @endforeach
+      @elseif(count($categories) == 1 && count($subCategories) == 1)
+        @foreach($categories as $category)
+          {{ $category->category_name }}
+        @endforeach
+      @else
+        Categories
+      @endif
+    </h2>
+    <hr />
     <div >
       <ul class="catalog-categories">
-        @foreach($categoryLinks as $category)
+      @if(count($categories) > 1)
+        @foreach($categories as $category)
           <li><a href="http://batteriesincluded.dev/catalog/{{ $category->category_name }}"> {{ $category->category_name }} </a></li>
         @endforeach
+      @elseif(count($categories) == 1)
+        @foreach($subCategoryLinks as $subCat)
+          <li><a href="http://batteriesincluded.dev/catalog/{{ $subCat->subcategory_name }}"> {{ $subCat->subcategory_name }} </a></li>
+        @endforeach
+      @elseif(count($categories) == 1 && count($subCategories) == 1)
+        @foreach($subCategoryLinks as $subCat)
+          <li><a href="http://batteriesincluded.dev/catalog/{{ $subCat->subcategory_name }}"> {{ $subCat->subcategory_name }} </a></li>
+        @endforeach
+      @endif
+
       </ul>
     </div>
   </div>
 
-  <div class="col-md-10 content">
-  <div class="col-md-12">
-    <h2>
-      @if(count($categories) == 1)
-        @foreach($categories as $category)
-          {{ $category->category_name }}
-          <hr />
-        @endforeach
-      @elseif(count($categories) == 0)
-        @foreach($subCategories as $subCategory)
-          {{ $subCategory->subcategory_name }}
-          <hr />
-        @endforeach
-      @else
-        Newest Items
-        <hr />
-      @endif
-    </h2>
-  </div>
+  <div class="col-md-9 content">
+    <div class="col-md-12">
+      <h2>
+        @if(count($categories) == 1 && count($subCategories) != 1)
+          @foreach($categories as $category)
+            {{ $category->category_name }}
+          @endforeach
+        @elseif(count($categories) == 1 && count($subCategories) == 1)
+          @foreach($subCategories as $subCategory)
+            {{ $subCategory->subcategory_name }}
+          @endforeach
+        @else
+          Newest Items
+        @endif
+      </h2>
+      <hr />
+    </div>
+
       @if(count($products) == 0)
         <div class="col-md-12">
           <p>There doesn't seem to be anything here!</p>
         </div>
       @else
         @foreach($products as $product)
-          <div class="col-md-3 product-tile">
+          <div class="col-md-4 product-tile">
           @if($product->price != 0.00)
             <span class="pull-right call-for-price">${{ $product->price }}</span>
           @else
             <span class="pull-right call-for-price">Call for price</span>
           @endif
-            <img src="http://placehold.it/100x100" class="img-responsive"/>
+            <img src="http://placehold.it/130x100" class="img-responsive"/>
             @if($product->subcategory->subcategory_name != 'Uncategorized')
               <small><b>Category:</b> <a href="http://batteriesincluded.dev/catalog/{{ $product->subcategory->subcategory_name }}"> {{ $product->subcategory->subcategory_name }} </a></small>
             @elseif($product->category->category_name)
