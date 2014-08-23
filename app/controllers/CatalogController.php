8d404a992151cdbb7eb5e8712fdb92c79cab290c
Devon <devon@Devon.(none)>
2014-08-22 19:10:45 -0700

    Created controller for showing specific category pages

--- app/controllers/CatalogController.php
+++ app/controllers/CatalogController.php
@@ -12,7 +12,23 @@ class CatalogController extends \BaseController {
         $data = Category::orderBy('category_name', 'ASC')->get(); // Get all category names and order them Alphabetically.
 		return View::make('catalog')
             ->with('products', Product::all())
-            ->with('categories', $data);
+            ->with('categories', $data)
+            ->with('categoryLinks', $data);
 
 	}
+
+    public function showCategory($category)
+    {
+        $products;
+        $categoryLinks = Category::orderBy('category_name', 'ASC')->get();
+        $data = Category::wherecategory_name($category)->get();
+        foreach($data as $i)
+        {
+          $products = Product::wherecategory_id($i->id)->get();
+        }
+        return View::make('catalog')
+            ->with('products', $products)
+            ->with('categories', $data)
+            ->with('categoryLinks', $categoryLinks);
+    }
 }
