946f3df9d697201277eb13d93197673130541bad
Phinocio <devon.r.mcclure@gmail.com>
2014-08-27 10:48:47 -0700

    Updated

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
+		$this->call('AdminSeeder');
 		$this->call('CategoriesSeeder');
 		$this->call('SubCategoriesSeeder');
 		$this->call('ProductsSeeder');
