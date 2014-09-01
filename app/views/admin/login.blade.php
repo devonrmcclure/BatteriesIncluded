9bf3b0cb120463fc4ff6914f9deb06e4afc23f36
Devon McClure <devon.r.mcclure@gmail.com>
2014-08-31 17:27:15 -0700

    Updated flash-message

--- app/views/admin/login.blade.php
+++ app/views/admin/login.blade.php
@@ -9,45 +9,41 @@
 @stop
 
 @section('content')
+<div class="col-md-4"></div>
+<div class="col-md-5">
+<div class="flash-message row">
+  @if(Session::has('flash-message'))
+      <div class="alert {{ Session::get('alert-class', 'alert-info') }} alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close/span></button>
+        {{ Session::get('flash-message') }}
+      </div>
+  @endif
+</div>
+    <h1>Please login!</h1>
+    <hr />
+    {{ Form::open(array('url' => 'http://batteriesincluded.dev/admin/login', 'class' => 'form-horizontal', 'id' => 'login-form', 'role' => 'form')) }}
 
-@if(Session::has('flash-message'))
-  <div class="row content">
-    <div class="col-md-3"></div>
-    <div class="flash-message col-md-6 text-center">
-        <span class="{{ Session::get('alert-class', 'alert-info') }}">
-            {{ Session::get('flash-message') }}
-        </span>
-    </div>
-    <div class="col-md-3"></div>
-  </div>
-@endif
-    <div class="col-md-4"></div>
-    <div class="col-md-5">
-        <h1>Please login!</h1>
-        <hr />
-        {{ Form::open(array('url' => 'http://batteriesincluded.dev/admin/login', 'class' => 'form-horizontal', 'id' => 'login-form', 'role' => 'form')) }}
+        <div class="form-group">
+          {{ Form::label('username', 'Userame', array('class' => 'col-sm-2 control-label')) }}
 
-            <div class="form-group">
-              {{ Form::label('username', 'Userame', array('class' => 'col-sm-2 control-label')) }}
+          <div class="col-sm-10">
+            {{ Form::text('username', '', array('class' => 'form-control', 'id' => 'username', 'placeholder' => 'Username')) }}
+          </div>
+        </div>
 
-              <div class="col-sm-10">
-                {{ Form::text('username', '', array('class' => 'form-control', 'id' => 'username', 'placeholder' => 'Username')) }}
-              </div>
-            </div>
+        <div class="form-group">
+          {{ Form::label('password', 'Password', array('class' => 'col-sm-2 control-label')) }}
 
-            <div class="form-group">
-              {{ Form::label('password', 'Password', array('class' => 'col-sm-2 control-label')) }}
-
-              <div class="col-sm-10">
-                {{ Form::password('password', array('class' => 'form-control', 'id' => 'password', 'placeholder' => 'Password')) }}
-              </div>
-            </div>
+          <div class="col-sm-10">
+            {{ Form::password('password', array('class' => 'form-control', 'id' => 'password', 'placeholder' => 'Password')) }}
+          </div>
+        </div>
 
 
-            {{ Form::submit('Submit', array('class' => 'btn btn-default submit-button submit-button', 'id' => 'submit-button', 'name' => 'submit')) }}
+        {{ Form::submit('Submit', array('class' => 'btn btn-default submit-button submit-button', 'id' => 'submit-button', 'name' => 'submit')) }}
 
-        {{ Form::close() }}
-    </div>
-    <div class="col-md-4"></div>
+    {{ Form::close() }}
+</div>
+<div class="col-md-4"></div>
 
 @stop
