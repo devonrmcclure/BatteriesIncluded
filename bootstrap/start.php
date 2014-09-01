94285e2e1db5a136bac530aef0c82c61aac4f110
Devon McClure <devon.r.mcclure@gmail.com>
2014-09-01 00:01:50 -0700

    Updated environments and deleted unsued files

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array
 (
-    'local' => array('Devon-PC', 'Devon')
+    'local' => array('Devon-PC', 'Devon'),
+    'prod' => array('devonmcclure')
 ));
 
 /*
