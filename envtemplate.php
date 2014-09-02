fc64c9c530b76c89a1e301c3b9de7f4bb5c87695
Phinocio <devon.r.mcclure@gmail.com>
2014-09-02 13:24:42 -0700

    Added env vars template

---
+++ envtemplate.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+    HOW TO USE:
+
+    1) Rename this file to the following:
+        a) On a local machine rename it to .env.local.php
+        b) On a produciton machine rename it to .env.php
+    2) Change the keys to the correct value for the environment you're working in.
+
+ */
+
+return array(
+
+    'MANDRILL_SECRET' => 'CHANGEME',
+    'URL'             => 'CHANGEME',
+    'MYSQL_PASS'      => 'CHANGEME',
+);
