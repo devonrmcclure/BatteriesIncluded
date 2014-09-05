88031882682475d5fe2ab7984469bf1254ee4050
Devon McClure <devon.r.mcclure@gmail.com>
2014-09-05 13:44:50 -0700

    Fixed padding issue on navbar

--- app/views/layouts/partials/nav.blade.php
+++ app/views/layouts/partials/nav.blade.php
@@ -1,6 +1,6 @@
       <!-- Site header and navigation -->
-      <div class="navbar top navbar-fixed-top" role="navigation">
-        <div class="container-fluid">
+      <div class="navbar navbar-inverted top navbar-fixed-top" role="navigation">
+        <div class="container">
           <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
               <span class="sr-only">Toggle navigation</span>
