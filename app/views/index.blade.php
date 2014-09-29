7f2fc1ac456388a76cf8412a839668fc094bc89c
Devon McClure <devon.r.mcclure@gmail.com>
2014-09-29 13:35:49 -0700

    Changed directory structure around to mimic that of the actual server

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -8,11 +8,11 @@
 
 	<div class="col-md-7 content">
 		<h2>Who We Are</h2>
-		<p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sagittis diam metus, a fermentum arcu fermentum ac. Sed sit amet lorem in leo laoreet pretium. Etiam auctor ornare mi a cursus. Morbi interdum nibh quis ante malesuada facilisis. Praesent pharetra sapien sapien, eu sollicitudin risus rutrum vitae. Quisque id orci eu libero volutpat semper in id velit. Duis pretium volutpat turpis non feugiat.</p>
+		<p class="text">Welcome! The site is currently undergoing a few upgrades, so please bare with us if there are any errors, or the site doesn't load properly at times. We will be back up fully very soon! In the mean time, why not take a look at our catalog, or use the contact page to ask us any questions you may have.</p>
 	</div>
 	<div class="col-md-5 content">
-		<h2>WHAT TO PUT HERE?!?</h2>
-		<p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sagittis diam metus, a fermentum arcu fermentum ac. Sed sit amet lorem in leo laoreet pretium. Etiam auctor ornare mi a cursus. Morbi interdum nibh quis ante malesuada facilisis. Praesent pharetra sapien sapien, eu sollicitudin risus rutrum vitae. Quisque id orci eu libero volutpat semper in id velit. Duis pretium volutpat turpis non feugiat.</p>
+		<!--<h2>WHAT TO PUT HERE?!?</h2>
+		<p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sagittis diam metus, a fermentum arcu fermentum ac. Sed sit amet lorem in leo laoreet pretium. Etiam auctor ornare mi a cursus. Morbi interdum nibh quis ante malesuada facilisis. Praesent pharetra sapien sapien, eu sollicitudin risus rutrum vitae. Quisque id orci eu libero volutpat semper in id velit. Duis pretium volutpat turpis non feugiat.</p>-->
 	</div>
 
 @stop
--- app/views/privacy-policy.blade.php
+++ app/views/privacy-policy.blade.php
@@ -7,12 +7,6 @@
 @section('content')
 
 	<div class="col-md-7 content">
-	    <h2>Who We Are</h2>
-	    <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sagittis diam metus, a fermentum arcu fermentum ac. Sed sit amet lorem in leo laoreet pretium. Etiam auctor ornare mi a cursus. Morbi interdum nibh quis ante malesuada facilisis. Praesent pharetra sapien sapien, eu sollicitudin risus rutrum vitae. Quisque id orci eu libero volutpat semper in id velit. Duis pretium volutpat turpis non feugiat.</p>
+        <h2>This page is currently under construction. Please check back shortly!</h2>
 	</div>
-	<div class="col-md-5 content">
-	    <h2>WHAT TO PUT HERE?!?</h2>
-	    <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sagittis diam metus, a fermentum arcu fermentum ac. Sed sit amet lorem in leo laoreet pretium. Etiam auctor ornare mi a cursus. Morbi interdum nibh quis ante malesuada facilisis. Praesent pharetra sapien sapien, eu sollicitudin risus rutrum vitae. Quisque id orci eu libero volutpat semper in id velit. Duis pretium volutpat turpis non feugiat.</p>
-	</div>
-
 @stop
