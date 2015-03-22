d77ffea59371ec0bc5bbd6dcb3568d14dc7c0f0e
Devon McClure <devon.r.mcclure@gmail.com>
2015-03-22 15:45:27 -0700

    Created collapseable menus for FAQ page

--- app/views/faq.blade.php
+++ app/views/faq.blade.php
@@ -6,8 +6,73 @@
 
 @section('content')
 
-	<div class="col-md-7 content">
-        <h2>This page is currently under construction. Please check back shortly!</h2>
+	<div class="col-md-2">Left</div>
+    <div class="col-md-8">
+        <div class="faq-question">
+            <a class="question" data-toggle="collapse" href="#question1" aria-expanded="false" aria-controls="question">
+                <div class="question-heading">
+                    <span class="glyphicon glyphicon-question-sign"></span>
+                        Do you ship items to customers?
+                    <span class="glyphicon glyphicon-menu-down pull-right" aria-hidden="true"></span>
+                </div>
+            </a>
+
+            <div class="collapse" id="question1">
+                <div class="well">
+                    No.
+                </div>
+            </div>
+        </div>
+
+        <div class="faq-question">
+            <a class="question" data-toggle="collapse" href="#question2" aria-expanded="false" aria-controls="question">
+                <div class="question-heading">
+                    <span class="glyphicon glyphicon-question-sign"></span>
+                        Do you ship items to customers?
+                    <span class="glyphicon glyphicon-menu-down pull-right" aria-hidden="true"></span>
+                </div>
+            </a>
+
+            <div class="collapse" id="question2">
+                <div class="well">
+                    No.
+                </div>
+            </div>
+        </div>
+
+        <div class="faq-question">
+            <a class="question" data-toggle="collapse" href="#question3" aria-expanded="false" aria-controls="question">
+                <div class="question-heading">
+                    <span class="glyphicon glyphicon-question-sign"></span>
+                        Do you ship items to customers?
+                    <span class="glyphicon glyphicon-menu-down pull-right" aria-hidden="true"></span>
+                </div>
+            </a>
+
+            <div class="collapse" id="question3">
+                <div class="well">
+                    No.
+                </div>
+            </div>
+        </div>
+
+        <div class="faq-question">
+            <a class="question" data-toggle="collapse" href="#question4" aria-expanded="false" aria-controls="question">
+                <div class="question-heading">
+                    <span class="glyphicon glyphicon-question-sign"></span>
+                        Do you ship items to customers?
+                    <span class="glyphicon glyphicon-menu-down pull-right" aria-hidden="true"></span>
+                </div>
+            </a>
+
+            <div class="collapse" id="question4">
+                <div class="well">
+                    No.
+                </div>
+            </div>
+        </div>
+
     </div>
+    <div class="col-md-2">Right</div>
 
 @stop
