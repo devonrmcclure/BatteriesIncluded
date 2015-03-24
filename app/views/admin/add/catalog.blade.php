2c262f04e54910a5bdb74e2f018f1b84cdb50823
Devon McClure <devon.r.mcclure@gmail.com>
2015-03-23 17:03:59 -0700

    Fixed missing < in span

--- app/views/admin/add/catalog.blade.php
+++ app/views/admin/add/catalog.blade.php
@@ -10,7 +10,7 @@
     <div class="flash-message row">
       @if(Session::has('flash-message'))
           <div class="alert {{ Session::get('alert-class', 'alert-info') }} alert-dismissible" role="alert">
-            <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close/span></button>
+            <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
             {{ Session::get('flash-message') }}
           </div>
       @endif
