1a8ffe274a87c5becca7b99d88ac6cd6b675b46d
Devon McClure <devon.r.mcclure@gmail.com>
2015-03-26 17:57:53 -0700

    Updated to reflect route names

--- app/controllers/CreateFAQController.php
+++ app/controllers/CreateFAQController.php
@@ -2,13 +2,19 @@
 
 class CreateFAQController extends \BaseController {
 
+    public function __construct()
+    {
+        $this->beforeFilter('auth');
+        $this->beforeFilter('pass_expired');
+    }
+
     /**
      * Show the create FAQ page.
      * @return [View] [views/admin/faq.blade.php]
      */
     public function showIndex()
     {
-        return View::make('admin.add.faq');
+        return View::make('admin.add.faqs');
     }
 
     /**
@@ -21,5 +27,23 @@ class CreateFAQController extends \BaseController {
         // > Check for completeness
         // > Post to DB
         // > Redirect to add FAQ page with success or error message.
+        $data = Input::all();
+        if($data['faq-question'] == '' || $data['faq-answer'] == '')
+        {
+            return Redirect::to($_ENV['URL'] . '/admin/add/faqs')
+                            ->with('flash-message', 'Please fill out all fields to add a FAQ!')
+                            ->with('alert-class', 'alert-danger');
+        } else {
+            $FAQ = new FAQ;
+            $FAQ->question = $data['faq-question'];
+            $FAQ->answer = $data['faq-answer'];
+            $FAQ->priority = $data['priority'];
+            $FAQ->created_at = Carbon::now();
+            $FAQ->updated_at = Carbon::now();
+            $FAQ->save();
+            return Redirect::to($_ENV['URL'] . '/admin/add/faqs')
+                            ->with('flash-message', 'FAQ has been successfully added!')
+                            ->with('alert-class', 'alert-success');
+        }
     }
 }
