f9a3832c098a8a7798bf0a59dff054aaa7046432
Devon McClure <devon.r.mcclure@gmail.com>
2015-04-10 13:49:26 -0700

    Fixed Typo

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -87,7 +87,7 @@
          <div class="row featurette">
            <div class="col-md-7">
              <h2 class="featurette-heading">Who We Are</h2>
-             <p class="lead">Welcome! The site is currently undergoing a few upgrades, so please bare with us if there are any errors, or the site doesn't load properly at times. We will be back up fully very soon! In the mean time, why not take a look at our catalog, or use the contact page to ask us any questions you may have.</p>
+             <p class="lead">Welcome! The site is currently undergoing a few upgrades, so please bear with us if there are any errors, or the site doesn't load properly at times. We will be back up fully very soon! In the mean time, why not take a look at our catalog, or use the contact page to ask us any questions you may have.</p>
            </div>
            <div class="col-md-5"><br /><br /><br /><br /><br />
              <img class="featurette-image img-responsive center-block" src="img/batt-inc-logo.JPG" alt="Batteries Included Logo">
