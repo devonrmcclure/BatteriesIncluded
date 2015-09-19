946657acd632adbdf34018976b47d13f08bd3e1f
Devon McClure <devon.r.mcclure@gmail.com>
2015-09-18 19:30:23 -0700

    Updated date formats displayed on admin page.

--- app/views/admin/index.blade.php
+++ app/views/admin/index.blade.php
@@ -9,9 +9,9 @@
     <div class="content-card col-md-10 col-md-offset-1">
         <h2>Info For <em>{{Auth::user()->username}}</em></h2>
 
-        <p><b>Created at:</b> {{Auth::user()->created_at}}</p>
-        <p><b>Last password change:</b> {{Auth::user()->last_password_change}} (passwords are required to be changed every 90 days!)</p>
-        <p><b>Next forced password change:</b> {{Auth::user()->last_password_change->addDays(90)}} ({{Carbon::now()->diffInDays(Auth::user()->last_password_change->addDays(90), false)}} Days)</p>
+        <p><b>Created at:</b> {{Auth::user()->created_at->format('F j, Y')}}</p>
+        <p><b>Last password change:</b> {{Auth::user()->last_password_change->format('F j, Y')}} (passwords are required to be changed every 90 days!)</p>
+        <p><b>Next forced password change:</b> {{Auth::user()->last_password_change->addDays(90)->format('F j, Y')}} ({{Carbon::now()->diffInDays(Auth::user()->last_password_change->addDays(90), false)}} Days)</p>
 
 
     </div>
