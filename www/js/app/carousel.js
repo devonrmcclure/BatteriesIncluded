aa4d447e54f56d923d357643ab499fefc1acc1ae
Devon McClure <devon.r.mcclure@gmail.com>
2015-10-10 16:55:37 -0700

    Added carousel.js

---
+++ www/js/app/carousel.js
@@ -0,0 +1,9 @@
+$(function() {
+
+	$('#carousel-item').first().addClass('active');
+	$('.carousel-indicators').children().first().addClass('active');
+	$('.carousel-img').attr('class', 'first-slide');
+
+	$
+
+});
