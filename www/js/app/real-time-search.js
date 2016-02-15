4b1305f5ed0e31caa6b9cea922088c5404bfeb70
Devon McClure <devonrmcclure@gmail.com>
2016-02-15 12:44:57 -0800

    Added filter results for manaing pages

---
+++ www/js/app/real-time-search.js
@@ -0,0 +1,13 @@
+$("#rt-search").keyup(function () {
+    var value = this.value.toLowerCase().trim();
+
+    $("table tr").each(function (index) {
+        if (!index) return;
+        $(this).find("td").each(function () {
+            var id = $(this).text().toLowerCase().trim();
+            var not_found = (id.indexOf(value) == -1);
+            $(this).closest('tr').toggle(!not_found);
+            return not_found;
+        });
+    });
+});
